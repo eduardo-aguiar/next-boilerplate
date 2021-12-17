effab005bdfbac744818781de53b23b1a78dc9af
Eduardo Martins <eduardo.martins@apinteractive.com.br>
2021-12-17 15:29:54 -0300

    MongoDB boilerplate

---
+++ src/utils/database.ts
@@ -0,0 +1,14 @@
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.DATABASE_URL!);
+
+export async function connect() {
+  try {
+    await client.connect();
+
+    const db = client.db('test');
+    return { db, client };
+  } finally {
+    // console.log("conectado mané")
+  }
+}
